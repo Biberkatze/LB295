c0001f8890e692fc45110031867c9549809fe68c
Biberkatze <89131189+Biberkatze@users.noreply.github.com>
2024-02-28 17:24:36 +0100

    Add files via upload

---
+++ LB295-Cedric/Controllers/DataModel/Jet.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LB295_Cedric.Controllers.DataModel
+{
+    public class ProductDataModel
+    {
+        [Key]
+        public Guid jet_id { get; set; }
+
+        [Required]
+        public string modellname { get; set; }
+
+        [Required]
+        public string Hersteller { get; set; }
+
+        [Required]
+        public string Erstflug { get; set; }
+    }
+}
+
---
+++ LB295-Cedric/Controllers/DataModel/JetDb.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LB295_Cedric.Controllers.DataModel
+{
+    public class JetDb : DbContext
+    {
+        public DbSet<ProductDataModel> Jets { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=JetDb;Trusted_Connection=True");
+        }
+    }
+}
