999b80b7af0ed4f41546c2a02a06f09b3b7fc739
Biberkatze <89131189+Biberkatze@users.noreply.github.com>
2024-02-28 17:21:28 +0100

    Add files via upload

---
+++ LB295-Cedric/Migrations/20240228141425_JetDbStarted.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LB295Cedric.Migrations
+{
+    /// <inheritdoc />
+    public partial class JetDbStarted : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Jets",
+                columns: table => new
+                {
+                    jet_id = table.Column<Guid>(nullable: false),
+                    modellname = table.Column<string>(nullable: false),
+                    Hersteller = table.Column<string>(nullable: false),
+                    Erstflug = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Jets", x => x.jet_id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Jets");
+        }
+    }
+}
